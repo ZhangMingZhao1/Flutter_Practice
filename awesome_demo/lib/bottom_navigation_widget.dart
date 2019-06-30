b3dd139df12053fddd7cd13f1136ff9858df3b57
ZhangMingZhao1 <1104272319@qq.com>
2019-06-30 23:54:50 +0800

    update：路由

--- awesome_demo/lib/bottom_navigation_widget.dart
+++ awesome_demo/lib/bottom_navigation_widget.dart
@@ -1,63 +1,83 @@
 import 'package:flutter/material.dart';
+import 'pages/smart_home.dart';
+import 'pages/skill.dart';
+import 'pages/person.dart';
+import 'pages/content_recommend.dart';
+
 class BottomNavigationWidget extends StatefulWidget {
-  @override
   _BottomNavigationWidgetState createState() => _BottomNavigationWidgetState();
 }
 
-
 class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
   final _BottomNavigationColor = Colors.blue;
+  int _currentIndex = 0;
+  List<Widget> list = List();
+
+  @override
+  void initState(){
+    list
+      ..add(SmartHome())
+      ..add(Skill())
+      ..add(Person())
+      ..add(ContentRecommend());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-            items:[
-              BottomNavigationBarItem(
+      body: list[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
                 icon:Icon(
                   Icons.home,
-                  color: _BottomNavigationColor,
+                  color:_BottomNavigationColor,
                 ),
                 title:Text(
-                  '智能家居',
-                  style: TextStyle(color: _BottomNavigationColor),
+                    'Home',
+                    style:TextStyle(color:_BottomNavigationColor)
                 )
-              ),
-              BottomNavigationBarItem(
-                  icon:Icon(
-                    Icons.email,
-                    color:_BottomNavigationColor,
-                  ),
-                  title:Text(
-                      '技能中心',
-                      style:TextStyle(color:_BottomNavigationColor)
-                  )
-              ),
-
-              BottomNavigationBarItem(
-                  icon:Icon(
-                    Icons.pages,
-                    color:_BottomNavigationColor,
-                  ),
-                  title:Text(
-                      '内容推荐',
-                      style:TextStyle(color:_BottomNavigationColor)
-                  )
-              ),
-
-              BottomNavigationBarItem(
-                  icon:Icon(
-                    Icons.airplay,
-                    color:_BottomNavigationColor,
-                  ),
-                  title:Text(
-                      '我的',
-                      style:TextStyle(color:_BottomNavigationColor)
-                  )
-              ),
-            ],
-            type:BottomNavigationBarType.fixed
-        )
+            ),
+            BottomNavigationBarItem(
+                icon:Icon(
+                  Icons.email,
+                  color:_BottomNavigationColor,
+                ),
+                title:Text(
+                    'Email',
+                    style:TextStyle(color:_BottomNavigationColor)
+                )
+            ),
+            BottomNavigationBarItem(
+                icon:Icon(
+                  Icons.pages,
+                  color:_BottomNavigationColor,
+                ),
+                title:Text(
+                    'Pages',
+                    style:TextStyle(color:_BottomNavigationColor)
+                )
+            ),
+            BottomNavigationBarItem(
+                icon:Icon(
+                  Icons.airplay,
+                  color:_BottomNavigationColor,
+                ),
+                title:Text(
+                    'AipPlay',
+                    style:TextStyle(color:_BottomNavigationColor)
+                )
+            ),
+          ],
+          currentIndex:_currentIndex,
+          onTap:(int index){
+            setState((){
+              _currentIndex= index;
+            });
+          },
+          type:BottomNavigationBarType.fixed
+      ),
     );
   }
 }
---
+++ awesome_demo/lib/pages/smart_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SmartHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar:AppBar(
+          title: Text('HOME'),
+        ),
+        body:Center(
+          child: Text('HOME'),
+        )
+    );
+  }
+}
